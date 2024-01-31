5c9540867f264864016b64a9460f30a7fdabf046
tuanptit2002 <tuantienti952002@gmail.com>
2024-01-31 08:56:20 +0700

    createUser-> createCart
    Signed-off-by: tuanptit2002 <tuantienti952002@gmail.com>

--- src/main/java/com/example/shop/Service/Implement/UserServiceImpl.java
+++ src/main/java/com/example/shop/Service/Implement/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.shop.Service.Implement;
 
+import com.example.shop.Entity.Cart;
 import com.example.shop.Entity.User;
 import com.example.shop.Repository.UserRepository;
+import com.example.shop.Service.CartService;
 import com.example.shop.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,6 +14,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    @Autowired
+    CartService cartService;
     @Override
     public User loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(username);
@@ -20,6 +25,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void createUser(User user){
-        userRepository.save(user);
+        User userCheck = userRepository.findByEmail(user.getEmail());
+        if(userCheck != null)
+        {
+            throw new RuntimeException("Tai khoan da ton tai");
+        }
+        User user1 = userRepository.save(user);
+        Cart cart = new Cart();
+        cart.setUser(user1);
+        cartService.create(cart);
     }
 }
